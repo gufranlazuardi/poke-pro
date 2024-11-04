81214a9e2cb499ae938292319cd03f21dce455c2
gufranlazuardi <gufranlazuardi@gmail.com>
2024-11-04 16:15:26 +0700

    fix: error deploymen ts

--- src/pages/pokemon/[id].tsx
+++ src/pages/pokemon/[id].tsx
@@ -1,74 +1,105 @@
-import DontHavePokemon from "@/components/DontHavePokemon";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { pokemonData } from "../api/sample-pokemon";
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
 
-interface PokemonData {
-  nickname?: string;
-  data_pokemon: {
-    title: string;
-    image: string;
-    type: string[];
-  };
-}
+const getDataFromLocalStorage = () => {
+  if (typeof window !== "undefined") {
+    const data = localStorage.getItem("myPokemon");
+    return data ? JSON.parse(data) : [];
+  }
+  return [];
+};
 
-const MyPokemon = () => {
-  const [myPokemons, setMyPokemons] = useState<PokemonData[]>([]);
+const DetailPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { toast } = useToast();
+  const [nickname, setNickName] = useState<string>();
+  const [pokemons, setPokemons] = useState(getDataFromLocalStorage());
+  const [catchPoke, setCatchPoke] = useState(false);
 
   useEffect(() => {
-    const data = localStorage.getItem("myPokemon");
-    if (data) {
-      setMyPokemons(JSON.parse(data));
-    }
-  }, []);
+    localStorage.setItem("myPokemon", JSON.stringify(pokemons));
+  }, [pokemons]);
 
-  const handleDelete = (nickname: string) => {
-    const filter = myPokemons.filter((e: any) => {
-      return e.nickname !== nickname;
+  const pokemon = pokemonData.find((p) => p.id === id);
+
+  const handleCatchPokemon = () => {
+    setCatchPoke(true);
+  };
+  const handleAddPokemon = () => {
+    const myPokemons = {
+      nickname,
+      data_pokemon: pokemon,
+    };
+    setPokemons([...pokemons, myPokemons]);
+    setNickName("");
+    setCatchPoke(false);
+    toast({
+      title: "Great Job!!!",
+      description: "Pokemon added to your My Pokemon list.",
+      variant: "default",
     });
-    setMyPokemons(filter);
-    localStorage.setItem("myPokemon", JSON.stringify(filter));
   };
 
-  if (myPokemons.length === 0) {
-    return <DontHavePokemon />;
+  if (!pokemon) {
+    return <p>Pokemon not found</p>;
   }
 
   return (
-    <div className="flex flex-wrap gap-4 p-4">
-      {myPokemons.map((pokemon, index) => (
-        <div
-          key={index}
-          className="w-60 p-4 border rounded-lg shadow-md"
-        >
-          <h3 className="text-2xl font-semibold text-center">
-            {pokemon.nickname}
-          </h3>
-          <img
-            src={pokemon.data_pokemon.image}
-            alt={pokemon.data_pokemon.title}
-            width={150}
-            height={150}
-            className="mx-auto"
-          />
-          <div className="text-center mt-2">
-            <p>Type:</p>
-            <ul>
-              {pokemon.data_pokemon.type.map((type, i) => (
-                <li key={i}>{type}</li>
-              ))}
-            </ul>
-          </div>
-          <Button
-            className="w-full mt-4"
-            variant={"destructive"}
-            onClick={() => handleDelete(pokemon.nickname!)}
-          >
-            Delete
-          </Button>
+    <>
+      <div className="flex flex-col items-center p-4">
+        <h2 className="text-4xl font-bold">{pokemon.title}</h2>
+        <img
+          src={pokemon.image}
+          alt={pokemon.title}
+          width={200}
+          height={200}
+        />
+        <div className="mt-4 gap-2 flex flex-col items-center">
+          <h3 className="text-2xl font-semibold">Type:</h3>
+          <ul>
+            {pokemon.type.map((type, index) => (
+              <li key={index} className="text-md">
+                {type}
+              </li>
+            ))}
+          </ul>
+          <Button onClick={handleCatchPokemon}>Catch</Button>
         </div>
-      ))}
-    </div>
+      </div>
+      {catchPoke && (
+        <div className="w-full flex justify-center absolute top-32">
+          <div className="w-[30rem] h-[30rem] border flex flex-col gap-8 justify-center items-center bg-white rounded-xl">
+            <h1 className="text-4xl font-bold">Congratulations!</h1>
+            <div className="flex flex-col items-center">
+              <p className="text-xl">You've caught</p>
+              <p className="font-bold text-2xl">{pokemon.title}</p>
+            </div>
+            <div className="flex flex-col items-center w-full px-16">
+              <p>Nickname</p>
+              <form className="flex flex-col items-center gap-4">
+                <input
+                  type="text"
+                  className="border w-full h-8 justify-center text-center rounded-md bg-white"
+                  onChange={(e) => setNickName(e.target.value)}
+                />
+                <Button
+                  type="button"
+                  onClick={handleAddPokemon}
+                  disabled={!nickname}
+                >
+                  Save
+                </Button>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
-export default MyPokemon;
+export default DetailPage;
